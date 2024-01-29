0f039a21b0c484cdd150a0bcca0e1c941b0c8a98
Mahaprasad Mohanty <mahaprasad003@gmail.com>
2024-01-29 11:38:05 +0530

    database updates

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Projects
+
+# projects_list = [
+#     {
+#         'id': '1',
+#         'title': 'Ecommerce Website',
+#         'description': 'Fully functional ecomm website',
+#     },
+#     {
+#         'id':'2',
+#         'title': 'Portfolio website',
+#         'description': 'Built a portfolio website',
+#     },
+#     {
+#         'id': '3',
+#         'title': 'Social Network',
+#         'description': 'Built a social network',
+#     }
+# ]
 
 # Create your views here.
 def projects(request):
-    return render(request, 'projects/projects.html')
+    projects = Projects.objects.all()
+    context = {
+        'projects' : projects
+    }
+    return render(request, 'projects/projects.html', context)
 
 def project(request,pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = Projects.objects.get(id=pk)
+    tag = projectObj.tags.all()
+    
+    context = {
+        'project' : projectObj,
+        'tags' : tag
+    }
+    return render(request, 'projects/single-project.html',context)
